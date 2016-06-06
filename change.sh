50c723e545b176dcc511eb8a2d17d98d4dc9dcc4
sumanmaity112 <suman.maity112@gmail.com>
2016-06-06 09:11:56 +0530

    Write script to change devEnvironment product to endTb or vise-versa

---
+++ change.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ $1 = "-E" ] || [ $1 = "-e" ] || [ $1 = "endtb" ] ; then
+    (
+        cd ~/Bahmni/vagrant
+        vagrant up
+        vagrant ssh -c "mysql -u root -ppassword openmrs < /bahmni/endtb_dump &&
+        sudo unlink /var/www/bahmni_config &&
+        sudo ln -s /bahmni/endtb-config /var/www/bahmni_config &&
+        sudo service openmrs restart"
+    )
+
+elif [ $1 = "-P" ] || [ $1 = "-p" ] || [ $1 = "product" ] ; then
+    (
+        cd ~/Bahmni/vagrant
+        vagrant up
+        vagrant ssh -c "mysql -u root -ppassword openmrs < /bahmni/mysql*_dump &&
+        sudo unlink /var/www/bahmni_config &&
+        sudo ln -s /bahmni/default-config /var/www/bahmni_config &&
+        sudo service openmrs restart"
+    )
+else
+    tput bold; tput setaf 6; echo "\033[4mUsage\033[0m:"
+    tput bold; tput setaf 2; echo "\t-P, -p, product -> Move to product environment"
+    tput bold; tput setaf 2; echo "\t-E, -e, endtb -> Move to EndTB environment"
+fi
+
