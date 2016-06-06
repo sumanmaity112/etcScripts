f9d14d6f738c55716700120259b9328c2d766ae1
sumanmaity112 <suman.maity112@gmail.com>
2016-06-06 18:48:44 +0530

    Write script to search word in files which matches pattern

---
+++ checkCapsInHeader.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+regex=${1:-".*[A-Z].*"}
+rowNumber=${2:-'1'}
+for file in `ls`; do
+    str=`awk ${rowNumber} $file`
+    IFS=',' read -ra terms <<< "$str"
+    for term in "${terms[@]}"; do
+        [[ $term =~ ^${regex}$ ]] && echo $term"    -->   "$file
+    done
+done
